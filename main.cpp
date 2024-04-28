a75f1f95ad7e3972a7bfedb869d0a25643018064
Nitel Muhtaroglu <nitel.muhtaroglu@ozyegin.edu.tr>
2024-04-28 18:18:18 +0300

    clean code for better readibility.

--- main.cpp
+++ main.cpp
@@ -36,26 +36,6 @@ bool TestNonHomogeniousMfcs();
 
 int main(int argc, char **args) {
   // Initialize the MPI environment
-  MPI_Init(nullptr, nullptr);
-
-  // Get the number of processes
-  int world_size;
-  MPI_Comm_size(MPI_COMM_WORLD, &world_size);
-
-  // Get the rank of the process
-  int world_rank;
-  MPI_Comm_rank(MPI_COMM_WORLD, &world_rank);
-
-  // Get the name of the processor
-  char processor_name[MPI_MAX_PROCESSOR_NAME];
-  int name_len;
-  MPI_Get_processor_name(processor_name, &name_len);
-
-  // Print off a hello world message
-  printf("Hello world from processor %s, rank %d out of %d processors\n",
-         processor_name, world_rank, world_size);
-
-  // Finalize the MPI environment.
   Mat Kdense;
   Mat K;
   PetscViewer fd;                        /* viewer */
@@ -76,91 +56,65 @@ int main(int argc, char **args) {
   PetscMPIInt rank;
   MatInfo matinfo;
   PetscInt Dnnz, Onnz;
-  ierr = PetscInitialize(&argc, &args, (char *) 0, help);
-  if (ierr) { return ierr; }
+  PetscInitialize(&argc, &args, (char *) 0, help);
   int world_size_{0};
   MPI_Comm_size(PETSC_COMM_WORLD, &world_size_);
-  ierr = MPI_Comm_rank(PETSC_COMM_WORLD, &rank);
-  printf("Hello world from: rank %d out of %d processes.\n", rank, world_size);
-  CHKERRQ(ierr);
+  MPI_Comm_rank(PETSC_COMM_WORLD, &rank);
 
   /* Determine files from which we read the linear systems. */
-  ierr = PetscOptionsGetString(NULL, NULL, "-f", file, PETSC_MAX_PATH_LEN, &flg);
-  CHKERRQ(ierr);
+  PetscOptionsGetString(NULL, NULL, "-f", file, PETSC_MAX_PATH_LEN, &flg);
   if (!flg) SETERRQ(PETSC_COMM_WORLD, 1, "Must indicate binary file with the -f option");
 
   /* Open binary file.  Note that we use FILE_MODE_READ to indicate
      reading from this file. */
-  ierr = PetscViewerBinaryOpen(PETSC_COMM_WORLD, file, FILE_MODE_READ, &fd);
-  CHKERRQ(ierr);
+  PetscViewerBinaryOpen(PETSC_COMM_WORLD, file, FILE_MODE_READ, &fd);
 
   /* Load the matrix; then destroy the viewer. */
-  ierr = MatCreate(PETSC_COMM_WORLD, &Kdense);
-  CHKERRQ(ierr);
-  ierr = MatSetOptionsPrefix(Kdense, "a_");
-  CHKERRQ(ierr);
-  ierr = MatSetFromOptions(Kdense);
-  CHKERRQ(ierr);
-  ierr = MatLoad(Kdense, fd);
-  CHKERRQ(ierr);
-  ierr = PetscViewerDestroy(&fd);
-  CHKERRQ(ierr);
-  ierr = MatGetSize(Kdense, &m, &n);
-  CHKERRQ(ierr);
-  ierr = MatGetInfo(Kdense, MAT_LOCAL, &matinfo);
-  CHKERRQ(ierr);
+  MatCreate(PETSC_COMM_WORLD, &Kdense);
+  MatSetOptionsPrefix(Kdense, "a_");
+  MatSetFromOptions(Kdense);
+  MatLoad(Kdense, fd);
+  PetscViewerDestroy(&fd);
+  MatGetSize(Kdense, &m, &n);
+  MatGetInfo(Kdense, MAT_LOCAL, &matinfo);
 
   /* Get a sparse matrix K by dumping zero entries of Kdense */
-  ierr = MatCreate(PETSC_COMM_WORLD, &K);
-  CHKERRQ(ierr);
-  ierr = MatSetSizes(K, m, n, PETSC_DECIDE, PETSC_DECIDE);
-  CHKERRQ(ierr);
-  ierr = MatSetOptionsPrefix(K, "asp_");
-  CHKERRQ(ierr);
-  ierr = MatSetFromOptions(K);
-  CHKERRQ(ierr);
-  ierr = MatSetType(K, MATMPIAIJ);
-  CHKERRQ(ierr);
+  MatCreate(PETSC_COMM_WORLD, &K);
+  MatSetSizes(K, m, n, PETSC_DECIDE, PETSC_DECIDE);
+  MatSetOptionsPrefix(K, "asp_");
+  MatSetFromOptions(K);
+  MatSetType(K, MATMPIAIJ);
   Dnnz = (PetscInt) matinfo.nz_used / m + 1;
   Onnz = Dnnz / 2;
   printf("Dnnz %d %d\n", Dnnz, Onnz);
-  CHKERRQ(ierr);
-  ierr = MatMPISBAIJSetPreallocation(K, 1, Dnnz, NULL, Onnz, NULL);
-  CHKERRQ(ierr);
+  MatMPISBAIJSetPreallocation(K, 1, Dnnz, NULL, Onnz, NULL);
   /* The allocation above is approximate, so we must set this option to be permissive.
    * Real code should preallocate exactly. */
-  ierr = MatSetOption(K, MAT_NEW_NONZERO_LOCATION_ERR, PETSC_FALSE);
-  CHKERRQ(ierr);
+  MatSetOption(K, MAT_NEW_NONZERO_LOCATION_ERR, PETSC_FALSE);
 
   /* Check zero rows */
-  ierr = MatGetOwnershipRange(Kdense, &rstart, &rend);
-  CHKERRQ(ierr);
+  MatGetOwnershipRange(Kdense, &rstart, &rend);
   nrows = 0;
   for (PetscInt row{rstart}; row < rend; row++) {
-    ierr = MatGetRow(Kdense, row, &ncols, &cols, &vals);
-    CHKERRQ(ierr);
+    MatGetRow(Kdense, row, &ncols, &cols, &vals);
     nnzA += ncols;
     norm = 0.0;
     for (int j{0}; j < ncols; ++j) {
       val = PetscAbsScalar(vals[j]);
       if (norm < val) { norm = norm; }
       if (val > dtol) {
-        ierr = MatSetValues(K, 1, &row, 1, &cols[j], &vals[j], INSERT_VALUES);
+        MatSetValues(K, 1, &row, 1, &cols[j], &vals[j], INSERT_VALUES);
         if (row != cols[j]) {
-          ierr = MatSetValues(K, 1, &cols[j], 1, &row, &vals[j], INSERT_VALUES);
+          MatSetValues(K, 1, &cols[j], 1, &row, &vals[j], INSERT_VALUES);
         }
-        CHKERRQ(ierr);
         nnzAsp++;
       }
     }
     if (!norm) { ++nrows; }
-    ierr = MatRestoreRow(Kdense, row, &ncols, &cols, &vals);
-    CHKERRQ(ierr);
+    MatRestoreRow(Kdense, row, &ncols, &cols, &vals);
   }
-  ierr = MatAssemblyBegin(K, MAT_FINAL_ASSEMBLY);
-  CHKERRQ(ierr);
-  ierr = MatAssemblyEnd(K, MAT_FINAL_ASSEMBLY);
-  CHKERRQ(ierr);
+  MatAssemblyBegin(K, MAT_FINAL_ASSEMBLY);
+  MatAssemblyEnd(K, MAT_FINAL_ASSEMBLY);
 
   percent = (PetscReal) nnzA * 100 / (m * n);
   PetscPrintf(PETSC_COMM_SELF,
@@ -174,52 +128,6 @@ int main(int argc, char **args) {
   percent = (PetscReal) nnzAsp * 100 / (m * n);
   PetscPrintf(PETSC_COMM_SELF, " [%d] Matrix K nnzAsp %d, %g percent\n", rank, nnzAsp, percent);
 
-  /* investigate matcoloring for K */
-  PetscBool Asp_coloring = PETSC_FALSE;
-  ierr = PetscOptionsHasName(NULL, NULL, "-Asp_color", &Asp_coloring);
-  CHKERRQ(ierr);
-  if (Asp_coloring) {
-    MatColoring mc;
-    ISColoring iscoloring;
-    MatFDColoring matfdcoloring;
-    PetscPrintf(PETSC_COMM_WORLD, " Create coloring of K...\n");
-    ierr = MatColoringCreate(K, &mc);
-    CHKERRQ(ierr);
-    ierr = MatColoringSetType(mc, MATCOLORINGSL);
-    CHKERRQ(ierr);
-    ierr = MatColoringSetFromOptions(mc);
-    CHKERRQ(ierr);
-    ierr = MatColoringApply(mc, &iscoloring);
-    CHKERRQ(ierr);
-    ierr = MatColoringDestroy(&mc);
-    CHKERRQ(ierr);
-    ierr = MatFDColoringCreate(K, iscoloring, &matfdcoloring);
-    CHKERRQ(ierr);
-    ierr = MatFDColoringSetFromOptions(matfdcoloring);
-    CHKERRQ(ierr);
-    ierr = MatFDColoringSetUp(K, iscoloring, matfdcoloring);
-    CHKERRQ(ierr);
-    ierr = ISColoringDestroy(&iscoloring);
-    CHKERRQ(ierr);
-    ierr = MatFDColoringDestroy(&matfdcoloring);
-    CHKERRQ(ierr);
-  }
-
-  /* Write K in binary for study - see ~petsc/src/mat/examples/tests/ex124.c */
-  PetscBool Asp_write = PETSC_FALSE;
-  ierr = PetscOptionsHasName(NULL, NULL, "-Asp_write", &Asp_write);
-  CHKERRQ(ierr);
-  if (Asp_write) {
-    PetscViewer viewer;
-    ierr = PetscPrintf(PETSC_COMM_SELF, "Write K into file K.dat ...\n");
-    CHKERRQ(ierr);
-    ierr = PetscViewerBinaryOpen(PETSC_COMM_WORLD, "K.dat", FILE_MODE_WRITE, &viewer);
-    CHKERRQ(ierr);
-    ierr = MatView(K, viewer);
-    CHKERRQ(ierr);
-    ierr = PetscViewerDestroy(&viewer);
-    CHKERRQ(ierr);
-  }
   PetscMasterStiffnessEquationAdaptee master_stiffness_equation_;
   master_stiffness_equation_.SetStiffnessMatrix(K);
 
@@ -244,18 +152,15 @@ int main(int argc, char **args) {
       constraints);
 
   master_stiffness_equation_.ApplyConstraints();
-  MPI_Finalize();
+  PetscFinalize();
   return 0;
   master_stiffness_equation_.Solve();
 
   //TestNonHomogeniousMfcs();
 
-  ierr = MatDestroy(&Kdense);
-  CHKERRQ(ierr);
-  ierr = MatDestroy(&K);
-  CHKERRQ(ierr);
-  ierr = PetscFinalize();
-  return ierr;
+  MatDestroy(&Kdense);
+  MatDestroy(&K);
+  PetscFinalize();
 }
 
 bool TestNonHomogeniousMfcs() {
